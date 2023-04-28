6bdb79b627ed954564afee5694f92ecf706871a6
Abbas Behrainwala <abbasbehrain95@gmail.com>
2023-04-29 00:47:29 +0530

    Created Data Ingestion

---
+++ src/components/data_ingestion.py
@@ -0,0 +1,46 @@
+import os
+import sys
+from src.exception import Custom_Exception
+from src.logger import logging
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from dataclasses import dataclass 
+
+
+@dataclass
+class DataIngestionConfig:
+    train_data_path:str=os.path.join('artifacts','train.csv')
+    test_data_path:str=os.path.join('artifacts','test.csv')
+    raw_data_path:str=os.path.join('artifacts','data.csv')
+
+class DataIngestion:
+    def __init__(self):
+        self.ingestion_config=DataIngestionConfig()
+
+    def initiate_data_ingestion(self):
+        logging.info("Data Ingestion has started.")
+        try:
+            df=pd.read_csv(r'C:\Users\Admin\ML_Projects\Employee_Churn_Prediction\src\notebook\data.csv')
+            logging.info('Reading the data has started')
+            os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
+
+            df.to_csv(self.ingestion_config.raw_data_path,index=False,header=True)
+            logging.info('Train Test Split has Started.')
+            train_set,test_set = train_test_split(df,
+                                                    test_size=0.25,
+                                                    random_state=42,
+                                                    )
+            train_set.to_csv(self.ingestion_config.train_data_path,index=False,header=True)
+            test_set.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
+            logging.info('Train test Split has been done successfully.')  
+            return (self.ingestion_config.train_data_path,self.ingestion_config.test_data_path)    
+
+        except Exception as e:
+            raise Custom_Exception(e,sys)
+
+if __name__=="__main__":
+    obj=DataIngestion()
+    obj.initiate_data_ingestion()
+
+
+
