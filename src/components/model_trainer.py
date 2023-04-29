fcf175f685ebe8d3c0061777e73fa8bafb63c681
Abbas Behrainwala <abbasbehrain95@gmail.com>
2023-04-29 20:15:24 +0530

    Model Hyperparameter Tuning

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -29,12 +29,40 @@ class ModelTrainer:
             models={
                 "Random Forest":RandomForestClassifier(),
                 "Decision Tree":DecisionTreeClassifier(),
-                "Gradiant Boosting":GradientBoostingClassifier(),
+                "Gradient Boosting":GradientBoostingClassifier(),
                 "KNeighbors Classifier":KNeighborsClassifier(),
                 "Catboost Classifer":CatBoostClassifier(verbose=False),
                 "Adaboost Classifier":AdaBoostClassifier()
             }
-            model_report:dict=evaluate_models(X_train=X_train,y_train=y_train,X_test=X_test,y_test=y_test,models=models)
+            params={
+                "Random Forest":{
+                    'n_estimators':[8,16,32,64,128,256]
+                },
+                "Decision Tree":{
+                    'criterion':["gini", "entropy", "log_loss"]
+                },
+                "Gradient Boosting":{
+                    # 'loss':['squared_error', 'huber', 'absolute_error', 'quantile'],
+                    'learning_rate':[.1,.01,.05,.001],
+                    'subsample':[0.6,0.7,0.75,0.8,0.85,0.9],
+                    # 'criterion':['squared_error', 'friedman_mse'],
+                    # 'max_features':['auto','sqrt','log2'],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "KNeighbors Classifier":{
+                    'n_neighbors':[5,7,9,11]
+                },
+                "Catboost Classifer":{
+                    "depth":[6,8,10],
+                    "iterations":[30,50,100],
+                    "learning_rate":[0.01,0.5,0.1]
+                },
+                "Adaboost Classifier":{
+                    'learning_rate':[0.01,0.05,0.1],
+                    'n_estimators':[8,16,32,64,128,256]
+                }
+            }
+            model_report:dict=evaluate_models(X_train=X_train,y_train=y_train,X_test=X_test,y_test=y_test,models=models,param=params)
             best_model_score=max(sorted(model_report.values()))
             best_model_name=list(model_report.keys())[list(model_report.values()).index(best_model_score)]
             best_model=models[best_model_name]
